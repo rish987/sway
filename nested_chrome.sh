865e2b755b663cd52e5a9e90f7fa637c72e642f0
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2023-06-08 17:13:36 +0200

    separate nested scratchpad sessions!

---
+++ nested_chrome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+tmpdir=$HOME/chrome$1/
+
+if [ -d "$tmpdir" ]; then
+    rm -rf $tmpdir
+fi
+
+rsync -av --delete --exclude=/Singleton* --exclude=/Session* $3 $tmpdir > /dev/null
+
+browser=$2
+
+shift 3
+$browser --user-data-dir=$tmpdir "$@"
---
+++ open_scratch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+configfile="$HOME/.config/sway/config-$1"
+
+if [ ! -f "$configfile" ]; then
+    exit
+fi
+
+swaymsg "[con_mark=$1] scratchpad show, mode passthrough" || (sway -c "$configfile" & PID=$! && sleep 1 && swaymsg "[pid=$PID] move to scratchpad, scratchpad show, mode passthrough, mark $1, resize set $2 $3")
