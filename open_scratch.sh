af45dfa3fee64e47974d44180280f97f22c94378
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2024-01-15 21:54:54 -0800

    scratchpad bug workaround

--- open_scratch.sh
+++ open_scratch.sh
@@ -5,4 +5,6 @@ if [ ! -f "$configfile" ]; then
     exit
 fi
 
+# echo "opening scratch: $1" >> ~/scratch-log
+
 swaymsg "[con_mark=$1] scratchpad show, mode passthrough" || (sway -c "$configfile" & PID=$! && sleep 1 && swaymsg "[pid=$PID] move to scratchpad, scratchpad show, mode passthrough, mark $1, resize set $2 $3")
