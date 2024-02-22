3ef31278c161e09d83be6a8f03ca3ea681825b20
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2024-02-22 10:28:36 +0100

    gammastep stuff

---
+++ gammastep.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+valuefile="/tmp/value.dat"
+
+# if we don't have a file, start at zero
+if [ ! -f "$valuefile" ]; then
+    value="off"
+# otherwise read the value from the file
+else
+    value=$(cat "$valuefile")
+fi
+
+# show it to the user
+echo "value: ${value}"
+temp=3500
+brightness=0.45
+
+if [ "$value" = "off" ]; then
+  gammastep -l 0:0 -o -b $brightness:$brightness -t $temp:$temp &
+  value="$!"
+else
+  kill -9 "$value"
+  value="off"
+fi
+
+echo "${value}" > "$valuefile"
