b1fe89669939ce91f0a3eeee67fbb642638f5528
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2024-03-12 17:37:01 +0100

    fix for missing password data in chrome

--- nested_chrome.sh
+++ nested_chrome.sh
@@ -5,7 +5,8 @@ if [ -d "$tmpdir" ]; then
     rm -rf $tmpdir
 fi
 
-rsync -av --delete --exclude=/Singleton* --exclude=/Session* $3/ $tmpdir > /dev/null
+rsync -av --delete --exclude=/Singleton* $3/ $tmpdir > /dev/null
+#rsync -av --delete --exclude=/Singleton* --exclude=/Session* $3/ $tmpdir > /dev/null
 
 browser=$2
 
